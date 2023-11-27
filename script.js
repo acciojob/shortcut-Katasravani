183861edb2a50dfaf7a1b6194cea36d034189b04
Katasravani <105917453+Katasravani@users.noreply.github.com>
2023-11-27 12:34:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,18 +8,18 @@ function shortcut(str1, str2) {
 
     // Swap the case of the initial letters
     const initialLetters = (firstLetterStr1 === firstLetterStr1.toUpperCase() && firstLetterStr1 === firstLetterStr1.toUpperCase())
-        ? firstLetterStr1.toLowerCase() + firstLetterStr2.toLowerCase()
-        : firstLetterStr1.toUpperCase() + firstLetterStr2.toUpperCase();
+        ? firstLetterStr1.toUpperCase() + firstLetterStr2.toUpperCase()
+        : firstLetterStr1.toLowerCase() + firstLetterStr2.toLowerCase();
 
     return initialLetters;
 }
 
 // Taking user input using prompt
-const userInput1 = prompt('Enter the first string:');
-const userInput2 = prompt('Enter the second string:');
+// const userInput1 = prompt('Enter the first string:');
+// const userInput2 = prompt('Enter the second string:');
 
-// Calling the shortcut function with user input
-const result = shortcut(userInput1, userInput2);
+// // Calling the shortcut function with user input
+// const result = shortcut(userInput1, userInput2);
 
-// Displaying the result
-alert(result);
+// // Displaying the result
+// alert(result);
